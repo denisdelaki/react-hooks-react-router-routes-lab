638cc72d66080b962595fb59fb663c407cdd17ed
denisdelaki <denisdelaki@gmail.com>
2022-07-21 23:29:15 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actor = actors.map((actor) => {
+    const shows = actor.movies;
+    return (
+      <div key={actor.name}>
+        <h2>{actor.name}</h2>
+        <ul>
+          {shows.map((movie, index) => (<li key={movie}>{movie}</li>))}
+        </ul>
+      </div>
+    )
+  })
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actor}
+    </div>
+  )
 }
-
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      {/*{code here}*/}
+      <NavLink to="/movies" >Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/" exact activeStyle={{ background: "red" }}>
+        Home
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
